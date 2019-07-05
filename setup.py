7da07ef2d950584f66c59bb79cc2f48a78d2e575
oldbay <old_bay@mail.ru>
2019-07-05 15:10:58 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='multi_map', 
-    version='0.2', 
-    packages=['multi_map'],
+    version='0.2.1', 
+    packages=[
+        'multi_map',
+        'multi_map.requests', 
+    ],
     scripts=['bin/multi_map'], 
     install_requires=[
         'mapscript_publisher', 
